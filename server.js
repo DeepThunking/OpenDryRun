44bc9eaac0a35b24a1d85814b85b2841934bfcba
Andrew Dennis <andrew@deepthunking.com>
2026-07-27 17:12:47 -0400

    Prepping for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { getCoordinatesAtBearing } from './utils/weatherMath.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Open-Meteo API Base URL
 const baseUrl = 'https://api.open-meteo.com/v1/forecast';
